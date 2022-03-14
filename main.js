c33bb3d4530cf9091ea2ab620e1eba68ad1ef854
ryana0 <78768691+dafydd69@users.noreply.github.com>
2022-03-13 17:35:05 -0700

    fixed some stuff made it responsive a little bit and added info cards

--- main.js
+++ main.js
@@ -3,22 +3,35 @@ const clipBL = document.querySelector('#clipBL')
 const clipTR = document.querySelector('#clipTR')
 const sonarLanding = document.querySelector('#sonarLanding')
 const navbar = document.querySelector('#navbar')
+const leftNum = (window.innerWidth - 251) / 32
+console.log(leftNum)
 window.addEventListener('scroll', () => {
-    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 6
+    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 8
     clipTR.style.clipPath = `polygon(${75 - ((scrollPercentage * 100) * 0.75)}% 0%, 100% 0%, 100% ${25 + ((scrollPercentage * 100) * 0.75)}%)`
     clipBL.style.clipPath = `polygon(0% ${75 - ((scrollPercentage * 100) * 0.75)}%, 0% 100%, ${25 + ((scrollPercentage * 100) * 0.75)}% 100%)`
-    navbar.style.opacity = scrollPercentage
+    navbar.style.opacity = scrollPercentage / 2
 
+    if(4 - Math.round(scrollPercentage * 100) / 100 <= 3) {
+        sonarLanding.style.fontSize = '3rem'
+    } else {
+        sonarLanding.style.fontSize = 4 - Math.round(scrollPercentage * 100) / 100 + 'rem'
+    }
 
     if(15 - (scrollPercentage * 100) / 15 <= 0) {
-        sonarLanding.style.top = '0.375rem'
+        sonarLanding.style.top = '0.25rem'
     } else {
         sonarLanding.style.top = 15 - (scrollPercentage * 100) / 15 + 'rem'
     }
 
-    if(52 - (scrollPercentage * 100) / 7.5 <= 0) {
-        sonarLanding.style.left = '1rem'
+    if(leftNum - (scrollPercentage * 100) * ((leftNum) / 850) <= 7) {
+        sonarLanding.style.left = '3rem'
+        console.log('1rem happened')
     } else {
-        sonarLanding.style.left = 52 - (scrollPercentage * 150) / 11.75 + 'rem'
+        if(sonarLanding.style.left != '3rem') {
+            console.log(leftNum - (scrollPercentage * 100) * ((leftNum) / 850))
+            sonarLanding.style.left = leftNum - (scrollPercentage * 100) * ((leftNum) / 750) + 'rem'
+        } else {
+            sonarLanding.style.left = sonarLanding.offsetLeft + (scrollPercentage * 100) * ((leftNum) / 750) + 'rem'
+        }
     }
 })
